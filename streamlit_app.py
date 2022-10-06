498f791ea6dc4ae4dc1c3b1106136bc1c83f1926
anukriti-ranjan <89630232+anukriti-ranjan@users.noreply.github.com>
2022-10-06 13:19:03 +0530

    disable hashing

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def load_ann():
 search_index=load_ann()
 
 @st.cache
-def load_model():
+def load_model(hash_funcs={"MyUnhashableClass": lambda _: None}):
 	  return SentenceTransformer(f"{BASE_DIR}/sent_bert_model")
 
 model = load_model()
